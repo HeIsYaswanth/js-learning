58a038754d98e430eb094afd3e256d3031eb2c28
HeIsYaswanth <82866582+HeIsYaswanth@users.noreply.github.com>
2023-12-24 05:59:20 +0000

    data types doc added

---
+++ 01.Basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+/* 
+Basically In JS we are having 5 types of data types. 
+They are: 
+
+1. Number
+2. String
+3. Boolean
+4. Object  (null is object data type)
+5. Undefined
+
+Lets discuss each data type.
+*/
+
+let num = 88;  //number data type
+let str= "string data type";  //string data type
+let bool = true;  //boolean data type
+let undef = undefined; //undefined data type even we allott undefined value of even without allotting any value it is undefined data type.
+let name = null;   //null is a object data type. null is basically used to say the name is empty. If we use 0 it will be number. So we use null data type.
+
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof bool);
+console.log(typeof undef);
+console.log(typeof name);
+
+// console.table([typeof num, typeof str, typeof bool, typeof undef, typeof name])
+
+
