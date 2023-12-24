891767010029c318a36e73908e648091379026c2
HeIsYaswanth <82866582+HeIsYaswanth@users.noreply.github.com>
2023-12-24 03:44:07 +0000

    variables learnt

---
+++ 01.Basics/variables.js
@@ -0,0 +1,12 @@
+/*
+
+In this code we are exploring the variables in the Java Script.
+
+*/ 
+
+let name = "Yash";         //variable declared with let will get only block scope and functional scope. It is recommended to use to declare variables.
+var age = "19";           //variable declared with var will get global scope. So try to avoid it as much as possible.
+const dob = "08-08-2004"; //variable assigned with const will not be able to modify.
+city = "Kadapa";
+
+console.table([name, age, dob, city]);
